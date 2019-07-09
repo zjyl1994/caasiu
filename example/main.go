9aefe5c7db2dcb344b39a167e2e89b035e5c8d1d
zjyl1994 <me@zjyl1994.com>
2019-07-09 15:37:17 +0800

    add basic json valid rule

---
+++ example/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+import "github.com/zjyl1994/caasiu"
+
+func main() {
+	fmt.Println("Caasiu example")
+	jsonStr := `
+	{
+		"string":"sdfsdfdsdsfsd",
+		"data1":{
+			"data2":{
+				"data3":45.7
+			},
+			"data4":67,
+			"data5":"7430893"
+		}
+	}
+	`
+	cs := caasiu.NewJSON([]byte(jsonStr), nil)
+	valid, errMsg := cs.Valid(map[string][]string{
+		"string":            []string{"string"},
+		"data1.data2.data3": []string{"integer"},
+		"data1.data4":       []string{"integer"},
+		"data1.data5":       []string{"string", "integer"},
+	})
+	fmt.Println("VALID", valid)
+	fmt.Println("ERROR", errMsg)
+}
